e85173c4963ffac1fcfe684455d23adf1a9563f6
vladimir.ivanov <knebekaizer@gmail.com>
2019-07-11 04:58:14 +0300

    add CXX_INCLUDES with cmd line args

--- src/main.cpp
+++ src/main.cpp
@@ -10,47 +10,42 @@
 using namespace reflang;
 using namespace std;
 
-namespace
+namespace {
+vector<string> GetFilesToProcess(CmdArgs &cmd_args, int &argc, char **&argv)
 {
-	vector<string> GetFilesToProcess(CmdArgs& cmd_args, int& argc, char**& argv)
-	{
-		vector<string> files;
-		bool wtf = false;
-		try
-		{
-			--argc;
-			++argv;
-			files = cmd_args.Consume(argc, argv);
-			if (files.empty())
-			{
-				cerr << "No input files specified." << endl;
-				wtf = true;
-			}
-		}
-		catch (const CmdArgs::Exception& error)
-		{
-			cerr << "Error: " << error.GetError() << endl;
+	vector<string> files;
+	bool wtf = false;
+	try {
+		--argc;
+		++argv;
+		files = cmd_args.Consume(argc, argv);
+		if (files.empty()) {
+			cerr << "No input files specified." << endl;
 			wtf = true;
 		}
-
-		if (wtf)
-		{
-			cout << "Reflang tool to generate reflection metadata.\n";
-			cout << "\n";
-			cout << "Usage: reflang [reflang_flags] -- [clang_flags]\n";
-			cout << "Where [reflang_flags] are any of the below, and [clang_flags] "
-				"are any flags supported by the libclang version installed\n";
-			cout << "\n";
-			cout << "Supported flags:\n";
-			cmd_args.PrintHelp();
-			exit(-1);
-		}
-
-		return files;
 	}
+	catch (const CmdArgs::Exception &error) {
+		cerr << "Error: " << error.GetError() << endl;
+		wtf = true;
+	}
+
+	if (wtf) {
+		cout << "Reflang tool to generate reflection metadata.\n";
+		cout << "\n";
+		cout << "Usage: reflang [reflang_flags] -- [clang_flags]\n";
+		cout << "Where [reflang_flags] are any of the below, and [clang_flags] "
+		        "are any flags supported by the libclang version installed\n";
+		cout << "\n";
+		cout << "Supported flags:\n";
+		cmd_args.PrintHelp();
+		exit(-1);
+	}
+
+	return files;
+}
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	CmdArgs cmd_args;
 	auto list_only = cmd_args.Register<bool>(
@@ -86,17 +81,13 @@ int main(int argc, char** argv)
 	options.include = "^(" + filter_include->Get() + ")$";
 	options.exclude = "^(" + filter_exclude->Get() + ")$";
 
-	if (list_only->Get())
-	{
+	if (list_only->Get()) {
 		auto names = parser::GetSupportedTypeNames(files, argc, argv, options);
-		for (const auto& it : names)
-		{
+		for (const auto &it : names) {
 			cout << it << endl;
 		}
-	}
-	else
-	{
-	    if (argc) TraceX(argv[0]);
+	} else {
+/*	    if (argc) TraceX(argv[0]);
 	    char* a[] = {  //! FIXME
 	            "-I/Library//Developer/CommandLineTools/usr/include/c++/v1",
                 "-I/Library//Developer/CommandLineTools/usr/lib/clang/10.0.1/include",
@@ -108,8 +99,8 @@ int main(int argc, char** argv)
 
 	    };
 	    argc = sizeof(a) / sizeof(*a);
-
         argv = a;
+*/
 		auto types = parser::GetTypes(files, argc, argv, options);
 		serializer::Options options;
 		options.include_path = reflang_include->Get();
