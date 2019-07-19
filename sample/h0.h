eccc1c5ed822441e14042f217ca261dd920ba8a5
Vladimir Ivanov <knebekaizer@gmail.com>
2019-07-19 10:43:48 +0300

    minor

--- sample/h0.h
+++ sample/h0.h
@@ -3,33 +3,13 @@
 
 typedef struct
 {
-//public:
+public:
 	int iPub;
 
-//	void foo();
+    void foo();
 
-	struct Inner {
-	    int innerInt;
-	} inner;
-
-//private:
-//	int iPriv;
+private:
+	int iPriv;
 } TheStruct;
 
 int bar(TheStruct* s);
-
-//#ifdef __cplusplus__
-struct TheStruct::Inner OtherStruct;
-//#else
-//struct Inner OtherStruct;
-//#endif
-
-//}
-/*
-ns::TheStruct theStruct;
-
-extern "C" {
-
-ns::TheStruct& xRef = theStruct;
-}
-*/
